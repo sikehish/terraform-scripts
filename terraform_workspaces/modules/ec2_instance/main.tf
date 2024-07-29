1b3fc00adea2eb8070a337eb089bdc36f93f45aa
sikehish <hisham0502@gmail.com>
2024-07-29 13:25:53 +0530

    TF project involving 3 workspaces

---
+++ terraform_workspaces/modules/ec2_instance/main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+variable "ami" {
+    type = string
+  description = "This is AMI for the instance"
+}
+
+variable "instance_type" {
+    type = string
+  description = "This is the instance type, for example: t2.micro"
+}
+
+resource "aws_instance" "example" {
+    ami = var.ami
+    instance_type = var.instance_type
+}
